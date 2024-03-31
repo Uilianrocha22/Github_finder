7b338ec944fe6c106328f4e7dbde06abfe1e2806
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-03-31 13:41:05 -0300

    adição dos componentes Error  e  User

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,9 @@
 import Search from "../components/Search";
+import User from "../components/User";
+import Error from "../components/Error";
 import { useState } from "react";
 
-type UserProps = {
+export type UserProps = {
   avatar_url: string;
   login: string;
   location: string;
@@ -11,11 +13,19 @@ type UserProps = {
 
 export default function Home() {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   async function loadUser(userName: string) {
+    setError(false);
+    setUser(null);
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -32,7 +42,17 @@ export default function Home() {
   return (
     <div>
       <Search loadUser={loadUser} />
-      {user && <p>{user.login}</p>}
+      {user && (
+        <User
+          avatar_url={user.avatar_url}
+          followers={user.followers}
+          following={user.following}
+          location={user.location}
+          login={user.login}
+          // Ou Fazer um spread no state user{...user}
+        />
+      )}
+      {error && <Error />}
     </div>
   );
 }
