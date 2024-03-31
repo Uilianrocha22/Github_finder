97b57ee867dfa17a6d406596dbe4b0bffac6ce75
Uilian Oliveira Rocha <117882699+Uilianrocha22@users.noreply.github.com>
2024-03-30 21:27:07 -0300

    foi adicionado o componente globals.css e a pasta routers com componente Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <>
       <div>
         <h1>Github_finder</h1>
+        <Outlet />
       </div>
     </>
   );
---
+++ src/routes/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Page home</h1>
+    </div>
+  );
+}
